d39b181e63c3d3736ab6d2cd05a584fd2b147eb3
Apaquet37 <apaquet37@charlottesvilleschools.org>
2022-01-11 20:37:23 +0000

    Camera code with effects, untested

--- Camera/camera_test02.py
+++ Camera/camera_test02.py
@@ -1,7 +1,21 @@
 import time
 import picamera
 
+val = 0
 effects = ["pastel", "solarize", "colorswap", "watercolor", "denoise"]
 
-for x in effects:
-	
+with picamera.PiCamera() as camera:
+	for x in effects:
+		print("Starting to take image")
+		camera.resolution = (1024, 768)
+		camera.start_preview()
+		y = effects[val]
+       		camera.image_effect = y
+       		camera.annotate_text = y 
+       		time.sleep(2)
+        	camera.capture(str(val)+effects[val]+".jpg")
+        	print("Done")
+        	val = val+1
+   	else:
+        	camera.close() 
+		print("Finished")
